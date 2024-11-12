3258c9f0c78d256f82441d603b1fc014f079181a
kdoan3 <kbdoan103@student.hbuhsd.edu>
2024-11-12 19:21:31 +0000

    fdafda

--- Main.java
+++ Main.java
@@ -4,14 +4,32 @@ public class Main
 {
   public static void main(String[] args)
   {
-    // You can test your method here
-    // System.out.println(isPrime(1));
-    // System.out.println(isPrime(17));
+    String sentence ="peter piper blah";
+    int count = 0;
+    for (int i = 0; i < sentence.length()-1; i++)
+    {
+      String pair = sentence.substring(i, i + 2);
+      if (pair.equals("pa") || pair.equals("pe") || pair.equals("pi") || pair.equals("po") || pair.equals("pu"))
+      {
+        count++;
+      }
+    }
+    System.out.println("Contains p after vowel" + count + " times.");
   }
 
-  public static boolean isPrime(int N)
-  {
-    // TODO: Replace and write your code below
-    return false;
+    public static String removeLetter(String Sentence)
+    {
+      String sentence = "peter piper picked a pack of pickle peppers."
+      String sentence2
+      int count = 0;
+      for (int i = 0; i < sentence.length(); i++)
+      {
+        String letter = sentence.substring(i, i + 1);
+        if (!(letter.equals("e") || letter.equals("t") || letter.equals("a") || letter.equals("i") ||letter.equals("o")))
+        {
+          sentence2 += letter;
+        }
+      }
+    }
   }
-}
+
